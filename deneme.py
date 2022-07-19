34cb0c3d3271731a3df50572cdfa44f2ce94315b
Baturay Arslan <baturay_arslan_fb@hotmail.com>
2022-07-19 15:45:13 +0300

    performance and readabilty adjusment.

--- deneme.py
+++ deneme.py
@@ -4,7 +4,7 @@ import asyncio
 import functools
 import time
 from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
 import re
 from zipfile import ZipFile
 
@@ -52,46 +52,62 @@ def check_key(key: str, config):
 def zip_file(path: Path):
     with ZipFile("s3-folder.zip", "a") as zip:
         zip.write(path)
+    print(f"Zipped :: {str(path)}", flush=True)
 
 
-async def main():
-    start_time = time.time()
-    s3 = boto3.client("s3")
-    print(config)
-    bucket_name = config["bucket"]
-    executor = ThreadPoolExecutor(max_workers=5)
-    process_executor = ProcessPoolExecutor(max_workers=4)
-    set_paths()
-    loop = asyncio.get_event_loop()
-
+async def download_and_zip(response: dict, session: dict):
     def aio(f):
         async def io_wrapper(*args, **kwargs):
             bounded_f = functools.partial(f, *args, **kwargs)
-            loop = asyncio.get_event_loop()
-            return await loop.run_in_executor(executor, bounded_f)
+            loop = session["loop"]
+            return await loop.run_in_executor(session["thread_executor"], bounded_f)
 
         return io_wrapper
 
-    download_file = aio(s3.download_file)
-    s3_paginator = s3.get_paginator("list_objects_v2")
-    s3_iterator = s3_paginator.paginate(
-        Bucket=bucket_name, Prefix=config["prefix"], PaginationConfig={"PageSize": 100})
+    download_file = aio(session["s3"].download_file)
 
     tasks = []
-
-    # check_response(response)
-    for page in s3_iterator:
-        for obj in page["Contents"]:
-            if check_key(obj["Key"], config):
-                path = create_file(obj["Key"])
-                tasks.append(
-                    (loop.create_task(download_file(bucket_name, obj["Key"], path)), obj["Key"]))
+    for obj in response["Contents"]:
+        if check_key(obj["Key"], config):
+            path = create_file(obj["Key"])
+            tasks.append(
+                (session["loop"].create_task(download_file(config["bucket"], obj["Key"], path)), obj["Key"]))
     zip_processes = []
     for task, key in tasks:
         await task
         print(f"{key} gonna zip")
-        zip_processes.append(process_executor.submit(zip_file,Path("s3-folder").joinpath(key)))
-        print(f"{key}  zipped.")
+        zip_processes.append(session["process_executor"].submit(zip_file, Path("s3-folder").joinpath(key)))
+
+    for _ in as_completed(zip_processes):
+        pass
+
+
+def set_session() -> dict:
+    return dict(
+        s3=boto3.client("s3"),
+        start_time=time.time(),
+        thread_executor=ThreadPoolExecutor(max_workers=5),
+        process_executor=ProcessPoolExecutor(max_workers=4),
+        loop=asyncio.get_event_loop()
+    )
+
+
+async def main():
+    start_time = time.time()
+    session = set_session()
+    set_paths()
+
+    response = session["s3"].list_objects_v2(Bucket=config["bucket"], Prefix=config["prefix"], MaxKeys=1000)
+
+    try:
+        while response:
+            next_response = session["thread_executor"].submit(session["s3"].list_objects_v2, Bucket=config["bucket"],
+                                                              MaxKeys=100,
+                                                              ContinuationToken=response["NextContinuationToken"])
+            await download_and_zip(response, session)
+            response = next_response.result()
+    except:
+        await download_and_zip(response, session)
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
